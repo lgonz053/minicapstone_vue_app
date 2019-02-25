66b61f0ab46a43cf238f5360a9904344eb01ab8d
Leo Gonzalez <lgonz053@gmail.com>
2019-02-25 09:38:35 -0600

    added working filter bar

--- src/views/ProductsIndex.vue
+++ src/views/ProductsIndex.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="home">
+    <div>
+      Filter Title: <input v-model="titleFilter">
+    </div>
 
     <h1>All Products</h1>
-    <div v-for="product in products">
+    <div v-for="product in filterBy(products, titleFilter, 'name')">
       <h2>{{ product.name }}</h2>
       <router-link v-bind:to="'/products/' + product.id">
         <img v-bind:src="product.image_url" v-bind:alt="product.name">
@@ -21,12 +24,14 @@
 
 <script>
 var axios = require('axios');
+import Vue2Filters from 'vue2-filters';
 
 export default {
   data: function() {
     return {
       products: [],
-      currentProduct: {}
+      currentProduct: {},
+      titleFilter: ''
     };
   },
   created: function() {
@@ -35,7 +40,8 @@ export default {
         this.products = response.data
     });
   },
-  methods: {}
+  methods: {},
+  mixins: [Vue2Filters.mixin]
 };
 </script>
  
